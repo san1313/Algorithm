a8c048509735689249c781b76ab3282e762c1a6a
san1313 <122683698+san1313@users.noreply.github.com>
2025-03-10 19:57:55 +0900

    [Bronze IV] Title: Quick Estimates, Time: 108 ms, Memory: 20352 KB -BaekjoonHub

---
+++ 백준/Bronze/13297. Quick Estimates/Quick Estimates.java
@@ -0,0 +1,21 @@
+public class Main {
+	public static void main(String[] args) throws Exception {
+		int n = nextInt();
+		for (int i = 0; i < n; i++) {
+			System.out.println(readLine().length());
+		}
+	}
+
+	static int nextInt() throws Exception {
+		int c, r = 0;
+		while ((c = System.in.read()) > 47) r = r * 10 + c - 48;
+		return r;
+	}
+
+	static String readLine() throws Exception {
+		byte[] buf = new byte[1 << 16];
+		int len = 0, c;
+		while ((c = System.in.read()) > 13) buf[len++] = (byte) c;
+		return new String(buf, 0, len);
+	}
+}
