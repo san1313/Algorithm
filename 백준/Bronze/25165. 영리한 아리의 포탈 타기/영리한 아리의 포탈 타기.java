5af6ddbd81eea4d1d457e49e6e487104c754c07a
san1313 <122683698+san1313@users.noreply.github.com>
2025-03-28 21:10:00 +0900

    [Bronze II] Title: 영리한 아리의 포탈 타기, Time: 96 ms, Memory: 14152 KB -BaekjoonHub

--- 백준/Bronze/25165. 영리한 아리의 포탈 타기/영리한 아리의 포탈 타기.java
+++ 백준/Bronze/25165. 영리한 아리의 포탈 타기/영리한 아리의 포탈 타기.java
@@ -1,11 +1,7 @@
 public class Main {
 	public static void main(String[] args) throws Exception {
-		int n = nextInt(), m = nextInt(), a = nextInt(), d = nextInt(), s1 = nextInt(), s2 = nextInt();
-		boolean res = false;
-		if (s1 != n) res = true;
-		else if (n % 2 == 0 && d == 0) res = true;
-		else if (n % 2 == 1 && d == 1) res = true;
-		System.out.println(res ? "NO..." : "YES!");
+		int n = nextInt(), m = nextInt(), a = nextInt(), d = nextInt(), s = nextInt();
+		System.out.println(s != n || (n % 2 == 0 && d == 0) || (n % 2 == 1 && d == 1) ? "NO..." : "YES!");
 	}
 
 	static int nextInt() throws Exception {
@@ -13,5 +9,4 @@ public class Main {
 		while ((c = System.in.read()) > 47) r = r * 10 + c - 48;
 		return r;
 	}
-
-	}
+}
