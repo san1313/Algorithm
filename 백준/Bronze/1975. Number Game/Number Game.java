a83d7a32d1d706fe14526019d8c81f8ee922dc7a
san1313 <122683698+san1313@users.noreply.github.com>
2025-04-07 16:46:49 +0900

    [Bronze II] Title: Number Game, Time: 332 ms, Memory: 15560 KB -BaekjoonHub

--- 백준/Bronze/1975. Number Game/Number Game.java
+++ 백준/Bronze/1975. Number Game/Number Game.java
@@ -4,10 +4,13 @@ public class Main {
 		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < t; i++) {
 			int n = nextInt(), r = 0;
-			for (int j = 2; j <= n; j++)
-				if (n % j == 0) {
-					r += calc(n, j);
+			for (int j = 2; j <= n; j++){
+				int tmp = n;
+				while (tmp % j == 0) {
+					r++;
+					tmp /= j;
 				}
+			}
 			sb.append(r).append("\n");
 		}
 		System.out.println(sb);
@@ -16,15 +19,6 @@ public class Main {
 	static int nextInt() throws Exception {
 		int c, r = 0;
 		while ((c = System.in.read()) > 47) r = r * 10 + c - 48;
-		return r;
-	}
-
-	static int calc(int n, int radix) {
-		int r = 0;
-		while (n > 0 && n % radix == 0) {
-			r++;
-			n /= radix;
-		}
 		return r;
 	}
 }
