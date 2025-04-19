f146894ed6ee091df210978194be24516b03cca9
san1313 <122683698+san1313@users.noreply.github.com>
2025-04-20 04:10:10 +0900

    [Bronze I] Title: 아스키 아트, Time: 208 ms, Memory: 19916 KB -BaekjoonHub

--- 백준/Bronze/17072. 아스키 아트/아스키 아트.java
+++ 백준/Bronze/17072. 아스키 아트/아스키 아트.java
@@ -1,21 +1,21 @@
 public class Main {
 	public static void main(String[] args) throws Exception {
 		int n = nextInt(), m = nextInt();
+		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < m; j++) System.out.print(func(nextInt(), nextInt(), nextInt()));
-			System.out.println();
+			for (int j = 0; j < m; j++) sb.append((char)func(nextInt(), nextInt(), nextInt()));
+			sb.append('\n');
 		}
+		System.out.println(sb);
 	}
 
-	static String func(int r, int g, int b) {
-		int intensity = r * 2126 + g * 7152 + b * 722;
-		String res = "";
-		if (intensity < 510000) res = "#";
-		else if (intensity < 1020000) res = "o";
-		else if (intensity < 1530000) res = "+";
-		else if (intensity < 2040000) res = "-";
-		else res = ".";
-		return res;
+	static int func(int r, int g, int b) {
+		int i = r * 2126 + g * 7152 + b * 722;
+		if (i < 510000) return 35;
+		else if (i < 1020000) return 111;
+		else if (i < 1530000) return 43;
+		else if (i < 2040000) return 45;
+		else return 46;
 	}
 
 	static int nextInt() throws Exception {
