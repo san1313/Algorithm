f3fbc15b70b4e343bacd2d6fad5ccd7997d6163e
san1313 <122683698+san1313@users.noreply.github.com>
2025-05-19 04:30:37 +0900

    [Bronze I] Title: 알고리즘 수업 - 선택 정렬 5, Time: 224 ms, Memory: 14564 KB -BaekjoonHub

---
+++ 백준/Bronze/23899. 알고리즘 수업 － 선택 정렬 5/알고리즘 수업 － 선택 정렬 5.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		int n = nextInt(), r = 0;
+		int[] a = new int[n];
+		int[] b = new int[n];
+		for (int i = 0; i < n; i++) a[i] = nextInt();
+		for (int i = 0; i < n; i++) b[i] = nextInt();
+		for (int i = n - 1; i > 0; i--) {
+			if (Arrays.equals(a, b)) {
+				r = 1;
+				break;
+			}
+			int max = 0, idx = -1;
+			for (int j = 0; j <= i; j++) {
+				if (max < a[j]) {
+					max = a[j];
+					idx = j;
+				}
+			}
+			if (idx != i) {
+				a[idx] = a[i];
+				a[i] = max;
+			}
+		}
+		if (Arrays.equals(a, b)) r = 1;
+		System.out.println(r);
+	}
+
+	static int nextInt() throws Exception {
+		int c, r = 0;
+		while ((c = System.in.read()) > 47) r = r * 10 + c - 48;
+		return r;
+	}
+}
