07da3e73d3534531296263c0073a90bc8764f311
san1313 <skan134679@gmail.com>
2025-05-31 19:38:56 +0900

    [Bronze I] Title: Inspiration, Time: 124 ms, Memory: 16044 KB -BaekjoonHub

--- 백준/Bronze/16715. Inspiration/Inspiration.java
+++ 백준/Bronze/16715. Inspiration/Inspiration.java
@@ -2,28 +2,17 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		int n = nextInt(), m = 0, r = 0;
 		for (int i = 2; i <= n; i++) {
-			int res = calcDigits(calcRadix(n, i));
-			if (m < res) {
-				m = res;
+			int sum = 0, tmp = n;
+			while (tmp > 0) {
+				sum += tmp % i;
+				tmp /= i;
+			}
+			if (m < sum) {
+				m = sum;
 				r = i;
 			}
 		}
 		System.out.println(m + " " + r);
-	}
-
-	static String calcRadix(int num, int radix) {
-		StringBuilder sb = new StringBuilder();
-		while (num > 0) {
-			sb.append((char) (num % radix + 48));
-			num /= radix;
-		}
-		return sb.reverse().toString();
-	}
-
-	static int calcDigits(String s) {
-		int r = 0;
-		for (char c : s.toCharArray()) r += c - 48;
-		return r;
 	}
 
 	static int nextInt() throws Exception {
