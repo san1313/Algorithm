3a1ef713b2ae8357aa29ec828678903b3952bfca
san1313 <skan134679@gmail.com>
2025-07-02 16:45:23 +0900

    [Bronze II] Title: 직사각형을 만드는 방법, Time: 104 ms, Memory: 13968 KB -BaekjoonHub

--- 백준/Bronze/8320. 직사각형을 만드는 방법/직사각형을 만드는 방법.java
+++ 백준/Bronze/8320. 직사각형을 만드는 방법/직사각형을 만드는 방법.java
@@ -1,13 +1,8 @@
 public class Main {
 	public static void main(String[] args) throws Exception {
-		int n = nextInt(), r = 0;
-		for (int i = 1; i <= n; i++) for (int j = 1; j <= n / i; j++) if (i <= j) r++;
+		int c, n = 0, r = 0;
+		while ((c = System.in.read()) > 47) n = n * 10 + c - 48;
+		for (int i = 1; i <= n; i++) for (int j = i; i * j <= n; j++) r++;
 		System.out.println(r);
-	}
-
-	static int nextInt() throws Exception {
-		int c, r = 0;
-		while ((c = System.in.read()) > 47) r = r * 10 + c - 48;
-		return r;
 	}
 }
