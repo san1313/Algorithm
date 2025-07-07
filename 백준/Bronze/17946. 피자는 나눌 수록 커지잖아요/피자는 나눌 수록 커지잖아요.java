70caa2dfb04d9823fb781694a04aebbd57b3804a
san1313 <skan134679@gmail.com>
2025-07-07 15:24:12 +0900

    [Bronze II] Title: 피자는 나눌 수록 커지잖아요, Time: 128 ms, Memory: 14604 KB -BaekjoonHub

---
+++ 백준/Bronze/17946. 피자는 나눌 수록 커지잖아요/피자는 나눌 수록 커지잖아요.java
@@ -0,0 +1,14 @@
+public class Main {
+	public static void main(String[] args) throws Exception {
+		int t = nextInt();
+		for (int i = 0; i < t; i++) {
+			System.out.println(1);
+		}
+	}
+
+	static int nextInt() throws Exception {
+		int c, r = 0;
+		while ((c = System.in.read()) > 47) r = r * 10 + c - 48;
+		return r;
+	}
+}
