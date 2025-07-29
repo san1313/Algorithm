feb5d653e474fb9e2e3a4aff0bab17a1d4222d3c
san1313 <skan134679@gmail.com>
2025-07-29 18:42:57 +0900

    [Bronze IV] Title: Prof. Ossama, Time: 396 ms, Memory: 123616 KB -BaekjoonHub

--- 백준/Bronze/10696. Prof． Ossama/Prof． Ossama.java
+++ 백준/Bronze/10696. Prof． Ossama/Prof． Ossama.java
@@ -1,10 +1,13 @@
-import java.math.BigInteger;
-
 public class Main {
 	public static void main(String[] args) throws Exception {
 		int c, t = 0;
 		while ((c = System.in.read()) > 47) t = t * 10 + c - 48;
-		for (int i = 1; i <= t; i++) System.out.printf("Case %d: %s\n", i, mod(readLine().split(" ")));
+		for (int i = 1; i <= t; i++) {
+			String[] s = readLine().split(" ");
+			long x = Long.parseLong(s[1]), r = 0, l = s[0].length();
+			for (int j = 0; j < l; j++) r = (r * 10 + s[0].charAt(j) - 48) % x;
+			System.out.printf("Case %d: %s\n", i, r);
+		}
 	}
 
 	static String readLine() throws Exception {
@@ -12,14 +15,5 @@ public class Main {
 		int len = 0, c;
 		while ((c = System.in.read()) > 13) buf[len++] = (byte) c;
 		return new String(buf, 0, len);
-	}
-
-	static BigInteger mod(String[] s) {
-		BigInteger r = BigInteger.ZERO;
-		BigInteger mod = new BigInteger(s[1]);
-		int l = s[0].length();
-		for (int i = 0; i < l; i++)
-			r = r.multiply(BigInteger.TEN).add(BigInteger.valueOf(s[0].charAt(i) - 48)).mod(mod);
-		return r;
 	}
 }
