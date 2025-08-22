bde7a972dd42cc0765049436d7c4119362dc20ed
san1313 <skan134679@gmail.com>
2025-08-22 15:22:21 +0900

    [Bronze II] Title: 시험 감독, Time: 212 ms, Memory: 22180 KB -BaekjoonHub

--- 백준/Bronze/13458. 시험 감독/시험 감독.java
+++ 백준/Bronze/13458. 시험 감독/시험 감독.java
@@ -1,19 +1,15 @@
 class Main {
 	public static void main(String[] args) throws Exception {
-		int n = nextInt();
-		int[] arr = new int[n];
-		for (int i = 0; i < n; i++) arr[i] = nextInt();
-		int b = nextInt(), c = nextInt();
-		long r = 0;
-		for (int i = 0; i < n; i++) {
-			arr[i] = Math.max(0, arr[i] - b);
-			r += 1 + (arr[i] + c - 1) / c;
-		}
+		int n = (int) nextLong();
+		long[] arr = new long[n];
+		for (int i = 0; i < n; i++) arr[i] = nextLong();
+		long b = nextLong(), c = nextLong(), r = 0;
+		for (int i = 0; i < n; i++) r += 1 + (Math.max(0, arr[i] - b) + c - 1) / c;
 		System.out.print(r);
 	}
 
-	static int nextInt() throws Exception {
-		int c, r = 0;
+	static long nextLong() throws Exception {
+		long c, r = 0;
 		while ((c = System.in.read()) > 47) r = r * 10 + c - 48;
 		return r;
 	}
