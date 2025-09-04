6ca3753d947f94ad964019625bb05b6040703b26
san1313 <skan134679@gmail.com>
2025-09-05 03:57:07 +0900

    [Bronze III] Title: 햄버거 정렬, Time: 96 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Bronze/32399. 햄버거 정렬/햄버거 정렬.java
+++ 백준/Bronze/32399. 햄버거 정렬/햄버거 정렬.java
@@ -1,7 +1,7 @@
 class Main {
 	public static void main(String[] args) throws Exception {
-		int[] a = new int[3];
-		for (int i = 0; i < 3; i++) a[i] = System.in.read();
-		System.out.println(a[0] == 40 && a[1] == 49 ? 0 : a[0] == 41 && a[1] == 49 ? 2 : 1);
+		int a = 0;
+		for (int i = 0; i < 2; i++) a = a * 100 + System.in.read();
+		System.out.print(a == 4049 ? 0 : a == 4149 ? 2 : 1);
 	}
 }
