3637c9dd6fb74d1bcbb7c99aab81a5d8d51fb017
san1313 <skan134679@gmail.com>
2025-09-24 03:19:13 +0900

    [Bronze V] Title: M-Climb, Time: 100 ms, Memory: 13960 KB -BaekjoonHub

---
+++ 백준/Bronze/34411. M－Climb/M－Climb.java
@@ -0,0 +1,11 @@
+class Main {
+	public static void main(String[] args) throws Exception {
+		System.out.print(nextInt() * nextInt());
+	}
+
+	static int nextInt() throws Exception {
+		int c, r = 0;
+		while ((c = System.in.read()) > 47) r = r * 10 + c - 48;
+		return r;
+	}
+}
