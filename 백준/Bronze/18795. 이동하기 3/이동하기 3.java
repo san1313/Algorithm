406dabcd277f7f40f0fc00e4fd4a00ea6bc25b83
san1313 <skan134679@gmail.com>
2025-10-26 22:47:51 +0900

    [Bronze II] Title: 이동하기 3, Time: 332 ms, Memory: 14332 KB -BaekjoonHub

---
+++ 백준/Bronze/18795. 이동하기 3/이동하기 3.java
@@ -0,0 +1,17 @@
+class Main {
+	public static void main(String[] args) throws Exception {
+		int n = nextInt() + nextInt();
+		long r = 0;
+		for (int i = 0; i < n; i++) {
+			r += nextInt();
+		}
+		System.out.println(r);
+	}
+
+
+	static int nextInt() throws Exception {
+		int c, r = 0;
+		while ((c = System.in.read()) > 47) r = r * 10 + c - 48;
+		return r;
+	}
+}
