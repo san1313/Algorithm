56c47a9a9c49df1bd3992d0e68c01b689bb82eef
san1313 <skan134679@gmail.com>
2025-11-18 02:16:04 +0900

    [Bronze V] Title: 대전과학고등학교를 사랑하십니까?, Time: 136 ms, Memory: 14732 KB -BaekjoonHub

--- 백준/Bronze/34691. 대전과학고등학교를 사랑하십니까？/대전과학고등학교를 사랑하십니까？.java
+++ 백준/Bronze/34691. 대전과학고등학교를 사랑하십니까？/대전과학고등학교를 사랑하십니까？.java
@@ -6,7 +6,7 @@ public class Main {
 	}
 
 	static String readLine() throws Exception {
-		byte[] buf = new byte[1 << 16];
+		byte[] buf = new byte[6];
 		int len = 0, c;
 		while ((c = System.in.read()) > 13) buf[len++] = (byte) c;
 		return new String(buf, 0, len);
