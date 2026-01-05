aab8a0507e384a6e293ec81e690ee6eb145af855
san1313 <skan134679@gmail.com>
2026-01-05 12:19:30 +0900

    [Bronze I] Title: 가희와 열리지 않는 건널목, Time: 108 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Bronze/27885. 가희와 열리지 않는 건널목/가희와 열리지 않는 건널목.java
+++ 백준/Bronze/27885. 가희와 열리지 않는 건널목/가희와 열리지 않는 건널목.java
@@ -2,16 +2,12 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		int c = nextInt(), h = nextInt(), r = 0;
 		boolean[] arr = new boolean[86400];
-		for (int i = 0; i < c; i++) {
-			int s = nextInt() * 60 * 60 + nextInt() * 60 + nextInt();
-			for (int j = s; j < s + 40; j++) arr[j] = true;
-		}
-		for (int i = 0; i < h; i++) {
-			int s = nextInt() * 60 * 60 + nextInt() * 60 + nextInt();
+		for (int i = 0; i < c + h; i++) {
+			int s = nextInt() * 3600 + nextInt() * 60 + nextInt();
 			for (int j = s; j < s + 40; j++) arr[j] = true;
 		}
 		for (boolean b : arr) r += !b ? 1 : 0;
-		System.out.println(r);
+		System.out.print(r);
 	}
 
 	static int nextInt() throws Exception {
