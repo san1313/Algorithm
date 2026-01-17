92333fd865e44044c86b1e1af839c7aa83e300e9
san1313 <skan134679@gmail.com>
2026-01-17 17:15:37 +0900

    [Bronze III] Title: Dominos, Time: 100 ms, Memory: 14072 KB -BaekjoonHub

---
+++ 백준/Bronze/15236. Dominos/Dominos.java
@@ -0,0 +1,15 @@
+public class Main {
+	public static void main(String[] args) throws Exception {
+		int n = nextInt(), r = 0;
+		for (int i = 0; i <= n; i++)
+			for (int j = 0; j <= i; j++)
+				r += i + j;
+		System.out.print(r);
+	}
+
+	static int nextInt() throws Exception {
+		int c, r = 0;
+		while ((c = System.in.read()) > 47) r = r * 10 + c - 48;
+		return r;
+	}
+}
