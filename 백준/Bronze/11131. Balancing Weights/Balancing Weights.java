ebafbfefc675b942537bb6baf8dc18d4fab83a52
san1313 <skan134679@gmail.com>
2026-01-25 19:43:01 +0900

    [Bronze III] Title: Balancing Weights, Time: 104 ms, Memory: 14032 KB -BaekjoonHub

---
+++ 백준/Bronze/11131. Balancing Weights/Balancing Weights.java
@@ -0,0 +1,20 @@
+public class Main {
+	public static void main(String[] args) throws Exception {
+		int t = nextInt();
+		for (int i = 0; i < t; i++) {
+			int n = nextInt(), a = 0;
+			for (int j = 0; j < n; j++) a += nextInt();
+			System.out.println(a == 0 ? "Equilibrium" : a < 0 ? "Left" : "Right");
+		}
+	}
+
+	static int nextInt() throws Exception {
+		int c, r = 0;
+		while ((c = System.in.read()) < 33);
+		boolean neg = (c == 45);
+		if (neg) c = System.in.read();
+		do r = r * 10 + c - 48;
+		while ((c = System.in.read()) > 47);
+		return neg ? -r : r;
+	}
+}
