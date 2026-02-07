441dec43f9d5fc31e4f4743bc7f5e4bcbc714f64
san1313 <skan134679@gmail.com>
2026-02-07 23:03:13 +0900

    [Bronze II] Title: 강력한 비밀번호, Time: 92 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Bronze/16944. 강력한 비밀번호/강력한 비밀번호.java
+++ 백준/Bronze/16944. 강력한 비밀번호/강력한 비밀번호.java
@@ -1,39 +1,17 @@
 public class Main {
 	public static void main(String[] args) throws Exception {
-		int c, n = 0, cnt = 0;
+		int c, n = 0, r = 0;
 		while ((c = System.in.read()) > 47) n = n * 10 + c - 48;
 		byte[] arr = new byte[n];
 		System.in.read(arr);
-		if (!hasNumber(arr)) cnt++;
-		if (!hasLowerCase(arr)) cnt++;
-		if (!hasUpperCase(arr)) cnt++;
-		if (!hasSpecial(arr)) cnt++;
-		System.out.print(n + cnt < 6 ? 6 - n : cnt);
-	}
-
-	static boolean hasNumber(byte[] arr) {
-		for (byte b : arr)
-			if (b >= 48 && b <= 57) return true;
-		return false;
-	}
-
-	static boolean hasLowerCase(byte[] arr) {
-		for (byte b : arr)
-			if (b >= 97 && b <= 122) return true;
-		return false;
-	}
-
-	static boolean hasUpperCase(byte[] arr) {
-		for (byte b : arr)
-			if (b >= 65 && b <= 90) return true;
-		return false;
-	}
-
-	static boolean hasSpecial(byte[] arr) {
 		String s = "!@#$%^&*()-+";
 		for (byte b : arr) {
-			if(s.indexOf(b) > -1) return true;
+			if (b >= 48 && b <= 57) r |= 1;
+			if (b >= 97 && b <= 122) r |= 2;
+			if (b >= 65 && b <= 90) r |= 4;
+			if (s.indexOf(b) > -1) r |= 8;
 		}
-		return false;
+		r = 4 - Integer.bitCount(r);
+		System.out.print(n + r < 6 ? 6 - n : r);
 	}
 }
