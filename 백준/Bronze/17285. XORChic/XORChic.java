5af7819e149a81c604f67ddbf92f64c4d36d2f0c
san1313 <skan134679@gmail.com>
2026-03-21 18:30:56 +0900

    [Bronze II] Title: XORChic, Time: 96 ms, Memory: 14132 KB -BaekjoonHub

--- 백준/Bronze/17285. XORChic/XORChic.java
+++ 백준/Bronze/17285. XORChic/XORChic.java
@@ -1,12 +1,9 @@
 public class Main {
 	public static void main(String[] args) throws Exception {
-		char[] buf = new char[101];
-		int len = 0, c, key;
-		c = System.in.read();
-		key = c ^ 'C';
-		do {
-			buf[len++] = (char) (c ^ key);
-		} while ((c = System.in.read()) > 13);
+		int[] buf = new int[100];
+		int len = 0, c = System.in.read(), key = c ^ 'C';
+		do buf[len++] = (c ^ key);
+		while ((c = System.in.read()) > 13);
 		System.out.print(new String(buf, 0, len));
 	}
 }
