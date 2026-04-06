4b678a8501038f909f18a319c23a4cd1dec29539
san1313 <skan134679@gmail.com>
2026-04-06 21:23:34 +0900

    [Bronze I] Title: 비숍 투어, Time: 104 ms, Memory: 14104 KB, Score: 35 point -BaekjoonHub

--- 백준/Bronze/23885. 비숍 투어/비숍 투어.java
+++ 백준/Bronze/23885. 비숍 투어/비숍 투어.java
@@ -1,25 +1,34 @@
-import java.io.*;
-import java.util.*;
-
 public class Main {
 	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		long N = Long.parseLong(st.nextToken());
-		long M = Long.parseLong(st.nextToken());
-
-		st = new StringTokenizer(br.readLine());
-		long sx = Long.parseLong(st.nextToken());
-		long sy = Long.parseLong(st.nextToken());
-
-		st = new StringTokenizer(br.readLine());
-		long ex = Long.parseLong(st.nextToken());
-		long ey = Long.parseLong(st.nextToken());
-
-		if ((sx + sy) % 2 == (ex + ey) % 2) {
-			System.out.println("YES");
-		} else {
+		long n = nextInt(), m = nextInt(), sx = nextInt(), sy = nextInt(), ex = nextInt(), ey = nextInt();
+		if ((sx + sy) % 2 != (ex + ey) % 2) {
 			System.out.println("NO");
+			return;
 		}
+		if (Math.abs(sx - ex) == Math.abs(sy - ey)) {
+			System.out.println("YES");
+			return;
+		}
+		long[] sums = {sx + sy, sx - sy};
+		long[] sums2 = {ex + ey, ex - ey};
+		for (long a : sums) {
+			for (long b : sums2) {
+				long x = (a + b) / 2;
+				long y = (a - b) / 2;
+				if ((a + b) % 2 == 0 && (a - b) % 2 == 0) { // 정수 좌표
+					if (x >= 1 && x <= n && y >= 1 && y <= m) {
+						System.out.println("YES");
+						return;
+					}
+				}
+			}
+		}
+		System.out.println("NO");
+	}
+
+	static int nextInt() throws Exception {
+		int c, r = 0;
+		while ((c = System.in.read()) > 47) r = r * 10 + c - 48;
+		return r;
 	}
 }
