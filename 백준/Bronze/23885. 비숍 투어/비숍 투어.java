194aa2e26ab15d40b1354aff78b874f3647a9665
san1313 <skan134679@gmail.com>
2026-04-06 21:29:46 +0900

    [Bronze I] Title: 비숍 투어, Time: 104 ms, Memory: 14216 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/23885. 비숍 투어/비숍 투어.java
+++ 백준/Bronze/23885. 비숍 투어/비숍 투어.java
@@ -1,35 +1,19 @@
 public class Main {
 	public static void main(String[] args) throws Exception {
 		long n = nextInt(), m = nextInt(), sx = nextInt(), sy = nextInt(), ex = nextInt(), ey = nextInt();
-		if ((sx + sy) % 2 != (ex + ey) % 2) {
-			System.out.println("NO");
-			return;
-		}
 		if (sx == ex && sy == ey) {
 			System.out.println("YES");
 			return;
 		}
-		if (Math.abs(sx - ex) == Math.abs(sy - ey)) {
-			System.out.println("YES");
+		if (((sx + sy) & 1) != ((ex + ey) & 1)) {
+			System.out.println("NO");
 			return;
 		}
-		long[][] lines1 = {{1, sx+sy}, {2, sx-sy}};
-		long[][] lines2 = {{1, ex+ey}, {2, ex-ey}};
-		for (long[] l1 : lines1) {
-			for (long[] l2 : lines2) {
-				if (l1[0] == l2[0]) continue;
-				long sum = (l1[0]==1 ? l1[1] : l2[1]);
-				long diff = (l1[0]==2 ? l1[1] : l2[1]);
-				if ((sum+diff)%2!=0 || (sum-diff)%2!=0) continue;
-				long x = (sum+diff)/2;
-				long y = (sum-diff)/2;
-				if (x>=1 && x<=n && y>=1 && y<=m) {
-					System.out.println("YES");
-					return;
-				}
-			}
+		if (n == 1 || m == 1) {
+			System.out.println("NO");
+			return;
 		}
-		System.out.println("NO");
+		System.out.println("YES");
 	}
 
 	static int nextInt() throws Exception {
