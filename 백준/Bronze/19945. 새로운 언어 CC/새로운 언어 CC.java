12b15354d41b0c1be3308c0e9ca563621db9cdcb
san1313 <skan134679@gmail.com>
2026-04-13 23:05:03 +0900

    [Bronze I] Title: 새로운 언어 CC, Time: 100 ms, Memory: 14088 KB -BaekjoonHub

---
+++ 백준/Bronze/19945. 새로운 언어 CC/새로운 언어 CC.java
@@ -0,0 +1,15 @@
+public class Main {
+	public static void main(String[] args) throws Exception {
+		System.out.println(Integer.toBinaryString(nextInt()).length());
+	}
+
+	static int nextInt() throws Exception {
+		int c, r = 0;
+		while ((c = System.in.read()) < 33);
+		boolean neg = (c == 45);
+		if (neg) c = System.in.read();
+		do r = r * 10 + c - 48;
+		while ((c = System.in.read()) > 47);
+		return neg ? -r : r;
+	}
+}
