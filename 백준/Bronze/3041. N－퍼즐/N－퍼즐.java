3f2fb6ecaa941940e0a3fdba821392e00ff1775a
san1313 <skan134679@gmail.com>
2026-04-26 12:09:43 +0900

    [Bronze I] Title: N-퍼즐, Time: 104 ms, Memory: 13964 KB -BaekjoonHub

--- 백준/Bronze/3041. N－퍼즐/N－퍼즐.java
+++ 백준/Bronze/3041. N－퍼즐/N－퍼즐.java
@@ -1,23 +1,14 @@
 public class Main {
 	public static void main(String[] args) throws Exception {
-		int[] arr = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', '.'};
-		int[] input = new int[16];
 		int r = 0;
 		for (int i = 0; i < 4; i++) {
-			for (int j = 0; j < 4; j++) input[i * 4 + j] = System.in.read();
+			for (int j = 0; j < 4; j++) {
+				int c = System.in.read() - 'A';
+				if (c == '.'-'A') continue;
+				r += Math.abs(i - c / 4) + Math.abs(j - c % 4);
+			}
 			System.in.read();
 		}
-		for (int i = 0; i < 16; i++) {
-			int c = input[i];
-			if (c == '.') continue;
-			int idx = indexOf(arr, c);
-			r += Math.abs(i / 4 - idx / 4) + Math.abs(i % 4 - idx % 4);
-		}
 		System.out.println(r);
-	}
-
-	public static int indexOf(int[] arr, int value) {
-		for (int i = 0; i < arr.length; i++) if (arr[i] == value) return i;
-		return -1;
 	}
 }
